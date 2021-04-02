2754c86797d3783fffd60907e31815869df1aeca
Amanibhavam <iam@defn.sh>
2021-04-02 01:23:59 -0700

    output subnet arns for RAM

--- outputs.tf
+++ outputs.tf
@@ -3,9 +3,19 @@ output "public_subnet_ids" {
   value       = aws_subnet.public.*.id
 }
 
+output "public_subnet_arns" {
+  description = "ARNs of the created public subnets"
+  value       = aws_subnet.public.*.arn
+}
+
 output "private_subnet_ids" {
   description = "IDs of the created private subnets"
   value       = aws_subnet.private.*.id
+}
+
+output "private_subnet_arns" {
+  description = "ARNs of the created private subnets"
+  value       = aws_subnet.private.*.arn
 }
 
 output "public_subnet_cidrs" {
